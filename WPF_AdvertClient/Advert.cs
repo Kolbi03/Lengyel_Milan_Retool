41496196ae5abd0efebce41f8c20fed5b1c3f091
Lengyel Milán <lengyel.milan.hu@gmail.com>
2023-12-01 14:07:41 +0100

    Add project files.

---
+++ WPF_AdvertClient/Advert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WPF_AdvertClient
+{
+    public class Advert
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("date")]
+        public string Date_Added { get; set; }
+
+        [JsonProperty("price")]
+        public int Price { get; set; }
+
+        [JsonProperty("seller")]
+        public string Seller { get; set; }
+        
+        [JsonProperty("interested")]
+        public bool Interested { get; set; }
+    }
+}
